8b27ad45dfb39f0e9fa7da717a2c03364715b2b2
David Rynn <davidrynn@gmail.com>
2022-09-14 05:32:36 -0400

    Initial Commit

---
+++ PassingDataDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PassingDataDemo
+//
+//  Created by David Rynn on 9/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PassingDataDemo/PassingDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataDemoApp.swift
+//  PassingDataDemo
+//
+//  Created by David Rynn on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PassingDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
